d319e790a65d3e0d87deb2ad51ef96b703f6796f
arturogood17 <arturo_good17@hotmail.com>
2026-03-15 16:17:26 +0100

    Tokenization of data. Helper function to tokenize the data. Data needs to be treated carefully. When comparing data, it is necessary to check if the data being compared is of the same type. Also, check unintentional reuse of variables. It might affect outcome of functions.

--- cli/search.py
+++ cli/search.py
@@ -5,11 +5,12 @@ def search(query: str, limit: int = DEFAULT_LIMIT) -> list[dict]:
     with open(file) as f:
         data = json.load(f)
     movies = []
-    processed_query = text_processor(query)
-    for x in data["movies"]:
-        processed_title = text_processor(x["title"])
-        if processed_query in processed_title:
-            movies.append(x)
+    processed_query = tokenization(text_processor(query))
+    for m in data["movies"]:
+        processed_title = text_processor(m["title"])
+        for token in processed_query:
+            if token in processed_title and not any(m["title"] == x["title"] for x in movies):
+                movies.append(m)
         if len(movies) >= limit:
             break
     return movies
@@ -17,3 +18,6 @@ def search(query: str, limit: int = DEFAULT_LIMIT) -> list[dict]:
 
 def text_processor(s: str) -> str:
     return s.lower().translate(str.maketrans('', '', string.punctuation))
+
+def tokenization(s: str) -> list[str]:
+    return list(filter(None, s.split(" ")))
