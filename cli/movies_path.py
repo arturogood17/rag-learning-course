f3607108394e6e2804069562e4b12686f01d2de1
arturogood17 <arturo_good17@hotmail.com>
2026-03-16 09:49:32 +0100

    Added path to the stopwords file, which is going to be used for removing stop words. Added function to remove stopwords.

--- cli/movies_path.py
+++ cli/movies_path.py
@@ -7,3 +7,5 @@ DEFAULT_LIMIT = 5
 parent = Path(os.path.dirname(os.path.realpath(__file__))).parent
 
 file = os.path.join(parent, "data", "movies.json")
+
+stop_words_file = os.path.join(parent, "data", "stopwords.txt")
--- cli/search.py
+++ cli/search.py
@@ -5,7 +5,7 @@ def search(query: str, limit: int = DEFAULT_LIMIT) -> list[dict]:
     with open(file) as f:
         data = json.load(f)
     movies = []
-    processed_query = tokenization(text_processor(query))
+    processed_query = stop_words(tokenization(text_processor(query)))
     for m in data["movies"]:
         processed_title = text_processor(m["title"])
         for token in processed_query:
@@ -21,3 +21,13 @@ def text_processor(s: str) -> str:
 
 def tokenization(s: str) -> list[str]:
     return list(filter(None, s.split(" ")))
+
+def stop_words(l: list[str]) -> list[str]:
+    with open(stop_words_file) as f:
+        content = f.read()
+    list_sw= content.splitlines()
+    new_tokens= []
+    for t in l:
+        if t not in list_sw:
+            new_tokens.append(t)
+    return new_tokens
