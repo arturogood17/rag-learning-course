b1312b350d664dd49b17a97af48d59d21f4e37d1
arturogood17 <arturo_good17@hotmail.com>
2026-03-18 23:59:28 +0100

    Moved all the functions from search to inverted_index to avoid circular import. Also, you need to tokenize and process (remove punctuation and lowercase all words). Added Load() method to load the index from disk and make the search fast

--- cli/invertex_index.py
+++ cli/invertex_index.py
@@ -1,7 +1,9 @@
-from search import *
 from movies_path import file, cache_docmap, cache_index
 import pickle
 import json, os
+import string #, json
+from movies_path import *
+from nltk.stem import PorterStemmer
 
 class InvertedIndex:
     def __init__(self) -> None:
@@ -9,7 +11,7 @@ class InvertedIndex:
         self.docmap = {} # {int:[str]}
     
     def __add_document(self, doc_id: int, text: str):
-        token_text = tokenization(text)
+        token_text = tokenization(text_processor(text))
         for t in token_text:
             if t not in self.index:
                 self.index[t] = set()
@@ -17,9 +19,12 @@ class InvertedIndex:
                                       #Es decir, si haces self.index[t] =self.index[t].add(), self.index[t] va a ser None
                                       #Porque estás reasignando el valor en el diccionario
 
-    def get_documents(self, term) -> list[str]:
+    def get_documents(self, term) -> list[int]:
         lower_version = term.lower()
-        return list(set((sorted(self.index[lower_version]))))
+        if lower_version not in self.index:
+            return []
+        return sorted(self.index[lower_version])
+
     
     def build(self):
         with open(file) as f:
@@ -28,11 +33,6 @@ class InvertedIndex:
             doc = f"{m['title']} {m['description']}"
             self.__add_document(m["id"], doc)
             self.docmap[m["id"]] = m
-    
-    def get(self):
-        print("Index: ", self.index)
-        print("Doc: ", self.docmap)
-
     
     def save(self):
         if not os.path.isdir(os.path.dirname(cache_docmap)):
@@ -45,3 +45,41 @@ class InvertedIndex:
             pickle.dump(self.docmap, cd)
         with open(cache_index, "wb") as ci:
             pickle.dump(self.index, ci)
+
+
+    def load(self):
+        try:
+            with open(cache_index, 'rb') as f:
+                self.index = pickle.load(f)
+            with open(cache_docmap, 'rb') as fc:
+                self.docmap = pickle.load(fc)
+        except Exception as e:
+            raise Exception("Ocurrió un error: ", e)
+
+
+
+def text_processor(s: str) -> str:
+    return s.lower().translate(str.maketrans('', '', string.punctuation))
+
+def tokenization(s: str) -> list[str]:
+    stemmer = PorterStemmer()
+    tokens = list(filter(None, s.split(" ")))
+    with open(stop_words_file) as f:
+        content = f.read()
+    list_sw= content.splitlines()
+    new_tokens= []
+    for t in tokens:
+        if t not in list_sw:
+            new_tokens.append(stemmer.stem(t))
+    return new_tokens
+
+def search_helper(II: InvertedIndex, query: str, limit: int = DEFAULT_LIMIT) -> list[dict]:
+    processed_query = tokenization(text_processor(query))
+    print(processed_query)
+    ids = []
+    results = []
+    for t in processed_query:
+        ids.extend(II.get_documents(t))
+    for i in range(limit):
+        results.append(II.docmap[ids[i]])
+    return results
--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-from search import search_func
-from invertex_index import InvertedIndex
+from invertex_index import *
 
 
 
@@ -17,21 +16,28 @@ def main() -> None:
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
+    build_parser = subparsers.add_parser("build", help="Gives ID of docs where a word appears")
     search_parser.add_argument("query", type=str, help="Search query")
-    search_parser = subparsers.add_parser("build", help="Gives ID of where a word appears")
+
+
 
     args = parser.parse_args()
 
+    Inverted_Index_Search = InvertedIndex()
+
     match args.command:
         case "search":
-            result = search_func(args.query)                      #Se hace así porque argparse convierte el add_argument("query") en un objeto         
-            pretty_printing_query_result(args.query, result) #al que se puede acceder directamente
+            try:
+                Inverted_Index_Search.load()
+            except:
+                print("Error al cargar el archivo")
+                return
+            results = search_helper(Inverted_Index_Search, args.query)
+            for r in results:
+                print(f"{r['id']} - {r['title']}")
         case "build":
-            Inverted_Index_Search = InvertedIndex()
             Inverted_Index_Search.build()
             Inverted_Index_Search.save()
-            ids = list(Inverted_Index_Search.get_documents('merida'))
-            print(f"First document for token 'merida' = {ids[0]}")
         case _:
             parser.print_help()
 
