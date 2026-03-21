b3f292ff5e9ff79437d3019726208cac1a612ae6
arturogood17 <arturo_good17@hotmail.com>
2026-03-21 14:15:32 +0100

    Added term_frequencies dict for term_frequency. It counts the times a word appears in a document. The more the term appears, more relevant it might be. This dict was is saved on disk and can be load from it. Fixed tokenization: str.split(" ") just removes simple spaces. You need to use str.split() to delete any kind of whitespace. Added function to calculate the frequency a term appears in a data set of documents (IDF). For the IDF, the term is important if it is rare, meaning, its frequency is not high (it is the inverse of term frequency)

--- cli/invertex_index.py
+++ cli/invertex_index.py
@@ -1,29 +1,38 @@
-from movies_path import file, cache_docmap, cache_index
-import pickle
+from movies_path import file, cache_docmap, cache_index, cache_term_frequency
+import pickle, math
 import json, os
 import string #, json
 from movies_path import *
+import collections
 from nltk.stem import PorterStemmer
 
 class InvertedIndex:
     def __init__(self) -> None:
         self.index = {} # {str:set(list[str])}
         self.docmap = {} # {int:[str]}
-    
+        self.term_frequencies = {} #{ID: Counter (diccionario optimizado para contar objetos)}
+
     def __add_document(self, doc_id: int, text: str):
         token_text = tokenization(text_processor(text))
         for t in token_text:
             if t not in self.index:
                 self.index[t] = set()
+            self.term_frequencies[doc_id] = collections.Counter(token_text)
             self.index[t].add(doc_id) #No hay que asignar la operación self.index[t].add() a nada porque esta da como resultado None
                                       #Es decir, si haces self.index[t] =self.index[t].add(), self.index[t] va a ser None
                                       #Porque estás reasignando el valor en el diccionario
 
-    def get_documents(self, term) -> list[int]:
-        lower_version = term.lower()
-        if lower_version not in self.index:
+    def get_documents(self, term: str) -> list[int]:
+        lower_version = tokenization(text_processor(term))
+        if lower_version[0] not in self.index:
             return []
-        return sorted(self.index[lower_version])
+        return sorted(self.index[lower_version[0]])
+    
+    def get_tf(self, doc_id: int, term: str) -> int:
+        token = tokenization(text_processor(term))
+        if len(token) > 1:
+            raise Exception("Only one word is permitted")
+        return self.term_frequencies[doc_id][token[0]]
 
     
     def build(self):
@@ -45,6 +54,8 @@ class InvertedIndex:
             pickle.dump(self.docmap, cd)
         with open(cache_index, "wb") as ci:
             pickle.dump(self.index, ci)
+        with open(cache_term_frequency, "wb") as ctf:
+            pickle.dump(self.term_frequencies, ctf)
 
 
     def load(self):
@@ -53,6 +64,8 @@ class InvertedIndex:
                 self.index = pickle.load(f)
             with open(cache_docmap, 'rb') as fc:
                 self.docmap = pickle.load(fc)
+            with open(cache_term_frequency, 'rb') as ftf:
+                self.term_frequencies = pickle.load(ftf)
         except Exception as e:
             raise Exception("Ocurrió un error: ", e)
 
@@ -63,7 +76,7 @@ def text_processor(s: str) -> str:
 
 def tokenization(s: str) -> list[str]:
     stemmer = PorterStemmer()
-    tokens = list(filter(None, s.split(" ")))
+    tokens = list(filter(None, s.split()))
     with open(stop_words_file) as f:
         content = f.read()
     list_sw= content.splitlines()
@@ -83,3 +96,9 @@ def search_helper(II: InvertedIndex, query: str, limit: int = DEFAULT_LIMIT) -> 
     for i in range(limit):
         results.append(II.docmap[ids[i]])
     return results
+
+
+# Inverse document frequency
+def idf_func(term: str, II: InvertedIndex) -> float:
+    p_term = tokenization(text_processor(term))[0]
+    return math.log((len(II.docmap) + 1) / (len(II.index[p_term]) + 1))
