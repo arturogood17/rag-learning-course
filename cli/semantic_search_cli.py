316ccc3a0d3aad4ced4ddeb4ccf68a13943b319b
arturogood17 <arturo_good17@hotmail.com>
2026-04-03 14:47:39 +0200

    Added command to embed a text

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -7,11 +7,15 @@ def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
     model_verify_suparser = subparsers.add_parser("verify", help="Verifies if the model is working")
+    embedding_suparser = subparsers.add_parser("embed_text", help="Embeds the given text")
+    embedding_suparser.add_argument("text", type=str, help= "Text to be embedded")
     args = parser.parse_args()
 
     match args.command:
         case "verify":
             verify_model()
+        case "embed_text":
+            embed_text(args.text)
         case _:
             parser.print_help()
 
