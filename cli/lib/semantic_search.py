19655f3023c1101c738bc96b79b3ef1e895b000f
arturogood17 <arturo_good17@hotmail.com>
2026-04-03 14:47:04 +0200

    Added method to SemanticSearch class to embed a text. It uses the tokenizer encode() from the embedder model. The tokenizer returns several information about the vector embedded. Added a function to be called by the CLI that embeds the provided text

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -4,9 +4,21 @@ class SemanticSearch:
     def __init__(self):
        self.model = SentenceTransformer("all-MiniLM-L6-v2")
 
+    def generate_embedding(self, text):
+        if not text or not text.strip():
+            raise ValueError("Texto vacío")
+        return self.model.encode([text])[0]
+
 
 
 def verify_model():
     embedder = SemanticSearch()
     print("Model loaded:" , embedder.model)
     print("Max sequence length:" , embedder.model.max_seq_length)
+
+def embed_text(text):
+    embedder = SemanticSearch()
+    embedding = embedder.generate_embedding(text)
+    print(f"Text: {text}")
+    print(f"First 3 dimensions: {embedding[:3]}")
+    print(f"Dimensions: {embedding.shape[0]}")
