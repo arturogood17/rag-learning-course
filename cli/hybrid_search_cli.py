9dcd6bdf8c2ee41a7abb1dfacfd13714dfec20c0
arturogood17 <arturo_good17@hotmail.com>
2026-04-22 14:45:56 +0200

    This commit constains two features: 1. Weighted command that normalizes the scores of keyword search and semantic search to create to be able to use the hybrid_score. Problems with the return of some functions made this features harder to implement that it should has been. 2. RRF command. It is another way to normalize scores. It ranks BM25 and Semantic Chunk scores and then it scores using a formula.

--- cli/hybrid_search_cli.py
+++ cli/hybrid_search_cli.py
@@ -1,5 +1,5 @@
 import argparse
-from lib.hybrid_search import normalize_command
+from lib.hybrid_search import *
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Hybrid Search CLI")
@@ -7,13 +7,25 @@ def main() -> None:
 
     normalize_subparser = subparsers.add_parser("normalize", help="Normalize the score between KS  and SM")
     normalize_subparser.add_argument(dest="list", nargs='*', type= float, help="List to be normalized")
+    weighted_search_subparser = subparsers.add_parser("weighted-search", help="Weighted search with configurable KM and SM")
+    weighted_search_subparser.add_argument("query", type=str, help= "Query to be searched")
+    weighted_search_subparser.add_argument("--alpha", type=float, default=0.5, help= "Configurable alpha that affects KM and SM")
+    weighted_search_subparser.add_argument("--limit", type=int, default=5, help= "Limit of results")
+    rrf_search_subparser = subparsers.add_parser("rrf-search", help="Weighted search with configurable KM and SM")
+    rrf_search_subparser.add_argument("query", type=str, help= "Query to be searched")
+    rrf_search_subparser.add_argument("-k", type=int, default=60, help= "Configurable k parameter that affects weight of the ranks")
+    rrf_search_subparser.add_argument("--limit", type=int, default=5, help= "Limit of results")
+    
 
     args = parser.parse_args()
 
     match args.command:
         case "normalize":
             normalize_command(args.list)
-        
+        case "weighted-search":
+            weighted_search_command(args.query, args.alpha, args.limit)
+        case "rrf-search":
+            rrf_search_command(args.query, args.k, args.limit)
         case _:
             parser.print_help()
 
--- cli/lib/hybrid_search.py
+++ cli/lib/hybrid_search.py
@@ -2,6 +2,8 @@ import os
 
 from inverted_index import InvertedIndex
 from .semantic_search import ChunkedSemanticSearch
+from movies_path import file, cache_index
+import json
 
 class HybridSearch:
     def __init__(self, documents):
@@ -10,7 +12,7 @@ class HybridSearch:
         self.semantic_search.load_or_create_chunk_embeddings(documents)
 
         self.idx = InvertedIndex()
-        if not os.path.exists(self.idx.index_path):
+        if not os.path.exists(cache_index):
             self.idx.build()
             self.idx.save()
 
@@ -18,21 +20,121 @@ class HybridSearch:
         self.idx.load()
         return self.idx.bm25_search(query, limit)
 
-    def weighted_search(self, query, alpha, limit=5):
-        raise NotImplementedError("Weighted hybrid search is not implemented yet.")
+    def weighted_search(self, query, alpha, limit):
+        BM25_results, SM_results = results_getter(self, query, limit)
+        BM25_normalization = []
+        SM_normalization= []
+        [BM25_normalization.append(v) for _,v in BM25_results.items()]
+        [SM_normalization.append(val["score"]) for val in SM_results]
+        BM25_normalized = normalize_command(BM25_normalization)
+        SM25_normalized = normalize_command(SM_normalization)
+        id_to_doc_scores_map = {}
+        index = 0
+        for i, _ in BM25_results.items():
+            id_to_doc_scores_map[i] = {
+                "document": self.semantic_search.document_map[i],
+                "BM25": BM25_normalized[index],
+                "SM": 0.0,
+            }
+            index += 1
+        index = 0
+        for i, d in enumerate(SM_results):
+            if d['id'] in id_to_doc_scores_map:
+                id_to_doc_scores_map[d['id']]["SM"] =  SM25_normalized[index]
+            else:
+                id_to_doc_scores_map[d['id']] = {
+                    "document": self.semantic_search.document_map[i],
+                    "BM25": 0.0,
+                    "SM": SM25_normalized[index],
+                }
+            index += 1
+        for k, val in id_to_doc_scores_map.items():
+            hybrid_s= hybrid_score(val["BM25"], val["SM"], alpha)           
+            id_to_doc_scores_map[k]["hybrid_score"] = hybrid_s
+        
+        sorted_results = sorted(id_to_doc_scores_map.items(), key = lambda x: x[1]["hybrid_score"], reverse=True)
+        return sorted_results[:limit]
 
-    def rrf_search(self, query, k, limit=10):
-        raise NotImplementedError("RRF hybrid search is not implemented yet.")
-    
+    def rrf_search(self, query, k, limit):
+        BM25_results, SM_results = results_getter(self, query, limit)
+        BM25_results = sorted(BM25_results.items(), key = lambda x: x[1], reverse=True)
+        SM_results.sort(key= lambda x: x['score'], reverse = True)
+        rank_map =  {}
+        for index, val  in enumerate(BM25_results, 1):
+            rank_map[val[0]] = {
+                'document': self.semantic_search.document_map[val[0]],
+                'BM25_rank': index,
+                'SM_rank': 0, 
+            }
+        for index, val in enumerate(SM_results):
+            if val["id"] in rank_map:
+                rank_map[val["id"]]["SM_rank"] = index
+            else:
+                rank_map[val["id"]] = {
+                    'document': self.semantic_search.document_map[val["id"]],
+                    'BM25_rank': 0,
+                    'SM_rank': index + 1,
+                }
+        for doc, val in rank_map.items():
+            if val["BM25_rank"] > 0 and val["SM_rank"] > 0:
+                rrf_score =  (1/(k + val["BM25_rank"])) + (1/(k + val["SM_rank"]))
+                rank_map[doc]["rrf_score"] = rrf_score
+            if val["BM25_rank"] == 0 and val["SM_rank"] >= 0:
+                rrf_score =  1/(k + val["BM25_rank"])
+                rank_map[doc]["rrf_score"] = rrf_score
+            if val["BM25_rank"] >= 0 and val["SM_rank"] == 0:
+                rrf_score =  1/(k + val["SM_rank"])
+                rank_map[doc]["rrf_score"] = rrf_score
+        
+        results = sorted(rank_map.items(), key= lambda x: x[1]["rrf_score"], reverse=True)
 
-def normalize_command(l: list[float]):
+        return results[:limit]
+
+def normalize_command(l: list[float]) -> list[float]:
     if len(l) == 0:
-        return
+        return []
     if min(l) == max(l):
         print(1.0)
-        return
+        return []
     min_val = min(l)
     max_val = max(l)
+    normalized_score = []
     for score in l:
-        normalized_score= (score - min_val) / (max_val - min_val)
-        print(f"* {normalized_score:.4f}")
+        new_score= (score - min_val) / (max_val - min_val)
+        normalized_score.append(new_score)
+    return normalized_score
+
+
+def hybrid_score(bm25_score, semantic_score, alpha):
+    return alpha * bm25_score + (1 - alpha) *semantic_score
+
+
+def weighted_search_command(query: str, alpha: float, limit: int):
+    with open(file, "r") as f:
+        movies = json.load(f)
+    hybrid_object = HybridSearch(movies["movies"])
+    hybrid_object.semantic_search.load_or_create_embeddings(movies["movies"])
+    results = hybrid_object.weighted_search(query, alpha, limit)
+    for index, r in enumerate(results, 1):
+        print(f"{index}. {r[1]['document']['title']}")
+        print(f"         Hybrid Score: {r[1]['hybrid_score']:.3f}")
+        print(f"         BM25: {r[1]['BM25']:.3f}, Semantic: {r[1]['SM']:.3f}")
+        print(f"         {r[1]['document']['description'][:limit]}")
+
+def rrf_search_command(query: str, k: int, limit: int):
+    with open(file, "r") as f:
+        movies = json.load(f)
+    hybrid_object = HybridSearch(movies["movies"])
+    results = hybrid_object.rrf_search(query, k, limit)
+    for index , result in enumerate(results, 1):
+        print(f"{index}. {result[1]['document']['title']}")
+        print(f"         RRF Score: {result[1]['rrf_score']:.3f}")
+        print(f"         BM25: {result[1]['BM25_rank']}, Semantic: {result[1]['SM_rank']}")
+        print(f"         {result[1]['document']['description'][:limit]}")
+
+
+
+def results_getter(h: HybridSearch, query: str, limit: int):
+    BM25_results = h._bm25_search(query, limit * 500)
+    SM_results = h.semantic_search.search_chunks(query, limit * 500)
+    return BM25_results, SM_results
