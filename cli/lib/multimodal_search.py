3ab4b0a170ff6acb1e0e0c35a44f5021019b14f4
arturogood17 <arturo_good17@hotmail.com>
2026-05-07 19:02:11 +0200

    Added a new command to the multimodal CLI: image_search. Also, added a new method to the MultimodalSearch class: search_with_image. It returns results after calculating cosine similarity. It may be faster if we add loading from disk. Right now is really slow because it has to do the embedding of the whole dataset first each time.

--- cli/lib/multimodal_search.py
+++ cli/lib/multimodal_search.py
@@ -1,18 +1,43 @@
 from PIL import Image
 from sentence_transformers import SentenceTransformer
+from .semantic_search import cosine_similarity
+from utils import file, load_file_json, DEFAULT_LIMIT
 
 class MultimodalSearch:
-    def __init__(self, model_name = "clip-ViT-B-32") -> None:
+    def __init__(self, docs: list[any], model_name = "clip-ViT-B-32") -> None:
         self.model = SentenceTransformer(model_name)
+        self.docs = docs
+        self.texts = [] #f"{doc['title']}: {doc['description']}"
+        for doc in self.docs:
+            self.texts.append(f"{doc['title']}: {doc['description']}")
+        self.text_embeddings = self.model.encode(self.texts, show_progress_bar=True)
 
     def embed_image(self, path: str):
         image = Image.open(path)
         return self.model.encode([image])[0]
+    
+    def search_with_image(self, path_img: str):
+        embedded_img = self.embed_image(path_img)
+        results = []
+        for index, txt_embedding in enumerate(self.text_embeddings):
+            cosine_simil = cosine_similarity(embedded_img, txt_embedding)
+            results.append({"doc_id": self.docs[index]["id"],
+                            "title": self.docs[index]["title"],
+                            "description": self.docs[index]["description"],
+                            "cosine_similarity":  cosine_simil})
+        return sorted(results, key= lambda x: x["cosine_similarity"], reverse=True)[:DEFAULT_LIMIT]
 
 
 def verify_image_embedding(img_path: str):
+    movies = load_file_json(file)
     if not img_path:
         return
-    multi_m_search = MultimodalSearch()
+    multi_m_search = MultimodalSearch(docs=movies["movies"])
     embedding = multi_m_search.embed_image(img_path)
     print(f"Embedding shape: {embedding.shape[0]} dimensions")
+
+def image_search_command(img_path: str):
+    movies = load_file_json(file)
+    multi_m_search = MultimodalSearch(docs=movies["movies"])
+    results = multi_m_search.search_with_image(img_path)
+    return results
--- cli/multimodal_search_cli.py
+++ cli/multimodal_search_cli.py
@@ -6,12 +6,21 @@ def main():
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
     verify_image_suparser = subparsers.add_parser("verify_image_embedding", help="Verifies the embedding of an image")
     verify_image_suparser.add_argument(dest="image_path", type=str, help= "Path to the image to be embedded")
+    image_search_suparser = subparsers.add_parser("image_search", help="Searches an image in the dataset")
+    image_search_suparser.add_argument(dest="image_path", type=str, help= "Path to the image to be embedded")
 
     args = parser.parse_args()
+    img_path = args.image_path.strip()
 
     match args.command:
         case "verify_image_embedding":
-            verify_image_embedding(args.image_path.strip())
+            verify_image_embedding(img_path)
+        case "image_search":
+            results = image_search_command(img_path)
+            for index, r in enumerate(results, 1):
+                print(f"{index}. {r["title"]} (similarity: {r["cosine_similarity"]:.3f})")
+                print(f"         {r["description"][:100]}...")
+                print()
         case _:
             parser.print_help()
 
